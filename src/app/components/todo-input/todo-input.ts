4b356a271aacb86357c10b05cbe476505f4f4d02
Vasantharaja-dev <vasantharajavijayakumar@gmail.com>
2025-06-22 11:44:09 +0530

    create todo-list and input components

---
+++ src/app/components/todo-input/todo-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input',
+  imports: [],
+  templateUrl: './todo-input.html',
+  styleUrl: './todo-input.scss'
+})
+export class TodoInput {
+
+}
