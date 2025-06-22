3d72e3f7cce55c7bb9c88cf6196af8b80b25f2fb
Vasantharaja-dev <vasantharajavijayakumar@gmail.com>
2025-06-22 11:45:02 +0530

    import todolist and input componet in ts and use it it html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoInput } from './components/todo-input/todo-input';
+import { TodoList } from './components/todo-list/todo-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoInput, TodoList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
